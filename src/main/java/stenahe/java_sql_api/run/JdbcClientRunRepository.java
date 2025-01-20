479d24dc7ab666aeeab51a3019070f727f9d85e1
Santiago Tena Hernandez <stenahe@gmail.com>
2025-01-20 21:35:14 +0100

    refactor

--- src/main/java/stenahe/java_sql_api/run/JdbcClientRunRepository.java
+++ src/main/java/stenahe/java_sql_api/run/JdbcClientRunRepository.java
@@ -1,18 +1,14 @@
 package stenahe.java_sql_api.run;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public class JdbcClientRunRepository {
 
-    private static final Logger log = LoggerFactory.getLogger(JdbcClientRunRepository.class);
     private final JdbcClient jdbcClient;
 
     public JdbcClientRunRepository(JdbcClient jdbcClient) {
